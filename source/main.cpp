f89e67a0f86554b6d2ade1d0575a7c25c8c8a069
Simon Sapin <simon.sapin@exyr.org>
2016-07-25 10:35:55 +0200

    Blinking a LED

--- source/main.cpp
+++ source/main.cpp
@@ -1,1 +1,21 @@
-int main() {}
+#include "gpio_api.h"
+#include "wait_api.h"
+
+const int PERIOD_MS = 500;
+const int ON_MS = 30;
+
+gpio_t row_1;
+gpio_t col_1;
+
+int main()
+{
+    gpio_init_out_ex(&row_1, ROW1, 1);
+    gpio_init_out(&col_1, COL1);
+    for (;;) {
+        gpio_write(&col_1, 0);
+        wait_ms(ON_MS);
+        gpio_write(&col_1, 1);
+        wait_ms(PERIOD_MS - ON_MS);
+    }
+}
+
