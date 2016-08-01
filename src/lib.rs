8a84f936f97c9c3c508b5ff4ffa0e9c70695687b
Simon Sapin <simon.sapin@exyr.org>
2016-08-02 00:35:48 +0200

    Move remaining boilerplate back into lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,16 @@
 #![no_std]
 #![feature(lang_items, asm)]
 
-#[macro_use] mod serial;
-pub mod boilerplate;  // Public to export symbols to the linker
-mod gpio;
 mod busy_loop;
+mod gpio;
 mod pins;
+#[macro_use] mod serial;
 
 const PERIOD_MS: u32 = 1000;
 const ON_MS: u32 = 50;
 
 #[no_mangle]
-pub unsafe extern fn main() -> ! {
+pub extern fn main() -> ! {
     serial::Serial::init();
     let row_2 = gpio::Pin::output(pins::ROW_2);
     let col_3 = gpio::Pin::output(pins::COL_3);
@@ -26,3 +25,20 @@ pub unsafe extern fn main() -> ! {
         busy_loop::wait_approx_ms(PERIOD_MS - ON_MS);
     }
 }
+
+#[lang = "panic_fmt"]
+extern fn panic_fmt(details: ::core::fmt::Arguments, file: &'static str, line: u32) -> ! {
+    println!("Panic at {}:{}, {}", file, line, details);
+    let row_2 = ::gpio::Pin::output(::pins::ROW_2);
+    let col_3 = ::gpio::Pin::output(::pins::COL_3);
+    row_2.set_high();
+    loop {
+        col_3.set_low();
+        ::busy_loop::wait_approx_ms(5);
+        col_3.set_high();
+        ::busy_loop::wait_approx_ms(200);
+    }
+}
+
+#[lang = "eh_personality"]
+extern fn eh_personality() {}
